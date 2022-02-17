16bff34548ea0f47811a2d1b75b2d374b8ed79f1
Gustavo Parreira <gustavotcparreira@gmail.com>
2022-02-17 17:21:31 +0000

    refactor: added default App component for unsupported platforms

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,51 +1,11 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import {
-  requestAuthorization,
-  AuthorizationStatus,
-  addFocusStatusChangeListener,
-  getCurrentFocusStatus,
-} from 'react-native-focus';
 
 export default function App() {
-  const [authorization, setAuthorization] = React.useState<
-    AuthorizationStatus | undefined
-  >();
-  const [focused, setFocused] = React.useState<boolean | undefined>(
-    getCurrentFocusStatus()
-  );
-
-  const authorizationText = React.useMemo<string>(() => {
-    switch (authorization) {
-      case AuthorizationStatus.Authorized:
-        return 'Authorized';
-      case AuthorizationStatus.Denied:
-        return 'Denied';
-      case AuthorizationStatus.NotDetermined:
-        return 'Not Determined';
-      case AuthorizationStatus.Restricted:
-        return 'Restricted';
-      default:
-        return '';
-    }
-  }, [authorization]);
-
-  React.useEffect(() => {
-    requestAuthorization().then(setAuthorization);
-  }, []);
-  React.useEffect(() => {
-    const unsubscribe = addFocusStatusChangeListener(setFocused);
-
-    return () => {
-      unsubscribe();
-    };
-  }, []);
-
   return (
     <View style={styles.container}>
-      <Text>Authorization: {authorizationText}</Text>
-      <Text>Status: {focused ? 'Focused' : 'Not Focused'}</Text>
+      <Text>'react-native-focus' is only available for iOS.</Text>
     </View>
   );
 }
